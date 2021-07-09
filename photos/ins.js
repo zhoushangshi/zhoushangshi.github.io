bd04942d8d1c1492c23aa679112a82c6dfef5294
zhoushangshi <zhoushangshi@163.com>
2021-07-09 23:25:03 +0800

    Site updated: 2021-07-09 23:24:57

--- photos/ins.js
+++ photos/ins.js
@@ -116,7 +116,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-		  var minSrc = 'https://gitee.com/zhoushangshi8/blog-back-up/raw/master/min_photos' + data.link[i];
+		  var minSrc = 'https://gitee.com/zhoushangshi8/blog-back-up/raw/master/min_photos/' + data.link[i];
 		  var src = 'https://gitee.com/zhoushangshi8/blog-back-up/raw/master/photos/'   + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
